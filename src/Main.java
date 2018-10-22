d4b860625fdb2811a55fb44bcd9452fd56aa5ad7
имя_польGefoS <_embers989@gmail.com>
2018-10-22 20:51:39 +0300

    Another commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Another commit");
+        System.out.println("Next Commit");
+    }
+}
